01888b4b0ceb84a3c1e6049427197a28485b27a2
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2024-04-29 12:35:42 -0600

    JellyBean is complete

--- JellyBean.java
+++ JellyBean.java
@@ -1,1 +1,19 @@
-
+public class JellyBean {
+    public static void main(String[] args) {
+        for(int i = 1; i < 52; i++) {
+            if(i % 5 == 0 && i % 2 == 0) {
+                System.out.print("Yuck! ");
+            }
+            else if(i % 5 == 0) {
+                System.out.print("Bean ");
+            }
+            else if(i % 2 == 0) {
+                System.out.print("Jelly ");
+            } 
+            else {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
